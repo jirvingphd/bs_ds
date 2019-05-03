b84bdbbb82bce78bf9d2542357b3a9ec07825952
James Irving <james.irving.phd@outlook.com>
2019-05-03 15:31:32 -0400

    Update conf.py
    changed module name in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -166,7 +166,7 @@ def run_apidoc(_):
 	import sys
 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 	cur_dir = os.path.abspath(os.path.dirname(__file__))
-	module = os.path.join(cur_dir,"..","labbookdb")
+	module = os.path.join(cur_dir,"..","bs_ds")
 	main(['-e', '-o', cur_dir, module, '--force'])
 
 def setup(app):
