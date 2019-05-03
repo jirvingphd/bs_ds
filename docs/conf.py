c192aab91ee3b7e41d6a957bf8ec98cf5199b9fe
James Irving <james.irving.phd@outlook.com>
2019-05-03 03:02:05 -0400

    Added sys.path.insert and os.path.abspath

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, 'D:\\Users\\James\\Dropbox (Personal)\\CODING\\My Python Code\\bs_ds\\bs_ds')
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('\\bs_ds'))
 
 
 # -- Project information -----------------------------------------------------
