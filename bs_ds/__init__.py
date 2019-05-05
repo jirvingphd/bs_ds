c6be65fba0fa0d9bea51b3874b0cf0a4a0aad4d1
James M. Irving <james.irving.phd@outlook.com>
2019-05-05 15:59:56 -0400

    V0.2.12

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
-__version__ = '0.2.11'
+__version__ = '0.2.12'
 
 import pandas as pd
 import numpy as np
