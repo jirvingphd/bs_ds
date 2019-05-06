7d6d62cce737b865d5adac7643dd20854538378e
James M. Irving <james.irving.phd@outlook.com>
2019-05-06 12:13:57 -0400

    updated multiplot

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
-__version__ = '0.3.7'
+__version__ = '0.3.8'
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
