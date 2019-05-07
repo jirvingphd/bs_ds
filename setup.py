21346a62ce20d21bcd9e96f473f75879e86564d6
James M. Irving <james.irving.phd@outlook.com>
2019-05-07 15:22:38 -0400

    v0.4.0 to deploy

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/bs_ds',
-    version='0.3.12',
+    version='0.4.0',
     zip_safe=False,
 )
