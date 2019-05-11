7140ca6965a1f2011bdb270899cafae0d790009a
James M. Irving <james.irving.phd@outlook.com>
2019-05-11 16:35:07 -0400

    Added confusion matrix

--- bs_ds/bs_ds.py
+++ bs_ds/bs_ds.py
@@ -1394,3 +1394,41 @@ def thick_pipe(features, target, n_components='mle',
 
 
 # HTML(f"<style>{CSS}</style>")
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=None,
+                          print_matrix=True):
+    """Check if Normalization Option is Set to True. If so, normalize the raw confusion matrix before visualizing
+    #Other code should be equivalent to your previous function."""
+    import itertools
+    import matplotlib.pyplot as plt
+    if cmap==None:
+        cmap = plt.get_cmap("Blues")
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
