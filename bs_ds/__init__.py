fce42cdf64933adb8d4642c0746320146e8a9b6a
James M. Irving <james.irving.phd@outlook.com>
2019-05-17 23:48:23 -0400

    bump to deploy saywhat fixed

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
-__version__ = '0.4.8'
+__version__ = '0.4.9'
 # import pandas as pd
 # import numpy as np
 # import matplotlib.pyplot as plt
