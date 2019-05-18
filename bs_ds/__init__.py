b053b13915184adc58af672e032c995a1a28b8c5
James M. Irving <james.irving.phd@outlook.com>
2019-05-18 01:21:49 -0400

    Removed importing from saywhat in init

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -17,7 +17,6 @@ __version__ = '0.4.9'
 # import scipy
 from .bs_ds import *
 from .bamboo import *
-from .saywhat import *
 print(f'View our documentation at https://bs-ds.readthedocs.io/en/latest/index.html')
 print(f'For convenient loading of standard modules :\n>> from bs_ds.imports import *\n')
 # print(f'Modules Displayed in Table Imported to Use\n(Available if used from bs_ds import *)\n')
