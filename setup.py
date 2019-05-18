1261cc99b89ccbafa743d85fc2f228cd39e6f468
James M. Irving <james.irving.phd@outlook.com>
2019-05-17 23:44:16 -0400

    moved nltk entry to same line in setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'pandas', 'seaborn', 'matplotlib', 'scikit-learn', 'pydotplus', 'scipy', 'xgboost' , 'IPython','pprint',
-'nltk'] #Click',
+requirements = ['numpy', 'pandas', 'seaborn', 'matplotlib', 'scikit-learn', 'pydotplus', 'scipy', 'xgboost' , 'IPython','pprint','nltk'] #Click',
 
 setup_requirements = [ ]
 
