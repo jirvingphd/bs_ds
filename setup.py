52082944b98da73c78c7402cdf821ca76bb1ec05
James M. Irving <james.irving.phd@outlook.com>
2019-05-17 23:08:00 -0400

    added nltk to requirements so pass deployment

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'pandas', 'seaborn', 'matplotlib', 'scikit-learn', 'pydotplus', 'scipy', 'xgboost' , 'IPython','pprint'] #Click',
+requirements = ['numpy', 'pandas', 'seaborn', 'matplotlib', 'scikit-learn', 'pydotplus', 'scipy', 'xgboost' , 'IPython','pprint',
+'nltk'] #Click',
 
 setup_requirements = [ ]
 
