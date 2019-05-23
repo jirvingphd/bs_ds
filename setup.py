f2923fe30bb419acc047150f3cb0cf1a5100f88f
James M. Irving <james.irving.phd@outlook.com>
2019-05-23 15:59:15 -0400

    v0.6.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/bs_ds',
-    version='0.5.4',
+    version='0.6.0',
     zip_safe=False,
 )
