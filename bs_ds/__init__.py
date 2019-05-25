d8ff26014f451949c0e37c7888485097bee0a1c7
James M. Irving <james.irving.phd@outlook.com>
2019-05-25 15:32:26 -0400

    deploy add_filtered_cols_to_df

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 from .bs_ds import *
 from .bamboo import *
