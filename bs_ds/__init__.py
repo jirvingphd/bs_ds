080e5d570328e1be8dd7a1c8ff3e2d4cf3935ae6
James M. Irving <james.irving.phd@outlook.com>
2019-05-25 21:53:20 -0400

    moved LabelLibrary back to bs_ds, will edit

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -9,6 +9,8 @@ __version__ = '0.6.3'
 from .bs_ds import *
 from .bamboo import *
 from .prettypandas import *
+from .glassboxes import *
+from .saycheese import *
 
 print(f'View our documentation at https://bs-ds.readthedocs.io/en/latest/index.html')
 print(f'For convenient loading of standard modules :\n>> from bs_ds.imports import *\n')
