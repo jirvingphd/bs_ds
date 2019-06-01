7591c740aeac143b483181c57468f670c5446280
James M. Irving <james.irving.phd@outlook.com>
2019-06-01 15:34:51 -0400

    bump to 0.7.0 to deploy

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
-__version__ = '0.6.6'
+__version__ = '0.7.0'
 
 from .bs_ds import *
 from .bamboo import *
