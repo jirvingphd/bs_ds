8f28afb0eed01ef1e3dc5d86eddbc8c4e06593cc
James M. Irving <james.irving.phd@outlook.com>
2019-06-01 20:27:35 -0400

    bump deploy .Clock

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 from .bs_ds import *
 from .bamboo import *
