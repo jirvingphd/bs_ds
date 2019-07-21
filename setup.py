93bb3451894d83e17edf48fd66cc69b6d2570d12
James M. Irving <james.irving.phd@outlook.com>
2019-07-21 14:17:34 -0400

    bump to deploy updates

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/bs_ds',
-    version='0.7.4',
+    version='0.8.0',
     zip_safe=False,
 )
