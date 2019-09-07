d872373d9f4a4e568a52c3c92735eb8c5eb84953
James M. Irving, Ph.D <james.irving.phd@outlook.com>
2019-09-07 12:47:34 -0400

    Bump version: 0.9.6 → 0.9.7

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
-__version__ = '0.9.6'
+__version__ = '0.9.7'
 
 from .bs_ds import *
 from .bamboo import *
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/bs_ds',
-    version='0.9.6',
+    version='0.9.7',
     zip_safe=False,
 )
