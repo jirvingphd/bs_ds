e6360e14f8043f64593467f19abeab881262ac16
James M. Irving <james.irving.phd@outlook.com>
2019-09-08 16:58:19 -0400

    Bump version: 0.9.9 → 0.9.10

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/bs_ds',
-    version='0.9.9',
+    version='0.9.10',
     zip_safe=False,
 )
