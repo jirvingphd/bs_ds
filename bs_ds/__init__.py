804261a29c007be0e75ca58b6b2b42c546e9018a
James M. Irving <james.irving.phd@outlook.com>
2019-09-30 18:52:56 -0400

    Bump version: 0.9.11 → 0.9.12

--- bs_ds/__init__.py
+++ bs_ds/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
-__version__ = '0.9.11'
+__version__ = '0.9.12'
 
 from .bs_ds import *
 from .bamboo import *
