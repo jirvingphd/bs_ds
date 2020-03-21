2d10f1591440c714a5daf3fe51e5fab2b66055e4
jirvingphd <james.irving.phd@gmail.com>
2020-03-21 14:10:38 -0400

    Bump version: 0.10.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/bs_ds',
-    version='0.10.0',
+    version='0.11.0',
     zip_safe=False,
 )
